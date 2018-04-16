980578d8c1d2032f865b8f213deb81ba15d9721f
Johnny Miller <johnnysviva@outlook.com>
2018-04-16 22:31:11 +0800

    Control the movement of snake

--- app/src/main/java/com/jm/greedysnake/ui/activity/MainActivity.java
+++ app/src/main/java/com/jm/greedysnake/ui/activity/MainActivity.java
@@ -1,30 +1,71 @@
 package com.jm.greedysnake.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jm.greedysnake.R;
 import com.jm.greedysnake.mvp.model.MainModel;
 import com.jm.greedysnake.mvp.presenter.MainPresenter;
 import com.jm.greedysnake.mvp.view.MainView;
 import com.jm.greedysnake.ui.base.BaseActivity;
+import com.jm.snakepanelview.GameType;
+import com.jm.snakepanelview.SnakePanelView;
 
 public class MainActivity
         extends BaseActivity<MainModel, MainView, MainPresenter>
         implements MainView {
+    private SnakePanelView snakePanelView;
+    private Button startOrPause;
+    private Button up;
+    private Button right;
+    private Button down;
+    private Button left;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        snakePanelView = findViewById(R.id.snakePanelView);
+        startOrPause = findViewById(R.id.startOrPause);
+        up = findViewById(R.id.up);
+        right = findViewById(R.id.right);
+        down = findViewById(R.id.down);
+        left = findViewById(R.id.left);
     }
 
     @Override
     public MainView createView() {
-        return null;
+        return this;
     }
 
     @Override
     public MainPresenter createPresenter() {
-        return null;
+        return new MainPresenter();
+    }
+
+    public void onClickStartOrPause(View view) {
+        snakePanelView.reStartGame();
+    }
+
+    public void onClickUp(View view) {
+        snakePanelView.setSnakeDirection(GameType.TOP);
+    }
+
+    public void onClickRight(View view) {
+        snakePanelView.setSnakeDirection(GameType.RIGHT);
+    }
+
+    public void onClickDown(View view) {
+        snakePanelView.setSnakeDirection(GameType.BOTTOM);
+    }
+
+    public void onClickLeft(View view) {
+        snakePanelView.setSnakeDirection(GameType.LEFT);
     }
 }
